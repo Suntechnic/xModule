b93619599aea9282b7c101621be6595bfb3c382b
Suntechnic <madzhugin@gmail.com>
2022-08-02 17:04:49 +0000

    фикс имени модуля в файле установки

--- install/index.php
+++ install/index.php
@@ -1,3 +1,16 @@
 <?
-if (!class_exists('X\Module\Module'))  include __DIR__.'/../lib/module.php';
-class x_module extends X\Module\Module {}
+if (!class_exists('X\Module\Module')) include __DIR__.'/../lib/module.php';
+class x_module extends \X\Module\Module
+// class x_module extends CModule
+{
+    /*
+    public $MODULE_ID = 'x.module';
+    
+    function __construct() {
+        $this->PARTNER_NAME = 'minisol';
+		$this->PARTNER_URI = 'https://minisol.ru';
+        parent::__construct();
+    }
+    */
+}
+
