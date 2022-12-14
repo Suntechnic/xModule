1a4aef3ed312f63de9222adc26f2a34666642552
Suntechnic <madzhugin@yandex.ru>
2022-12-14 12:28:11 +0300

    Update .env.php
    Добавлен обход мультисайтового рассирания битрикс.

--- .env.php
+++ .env.php
@@ -3,9 +3,15 @@ $tmpMODULE_PATH_ABS = __DIR__;
 $tmpMODULE_DIR = basename($tmpMODULE_PATH_ABS);
 $tmpMODULE_SPACE = explode('.',$tmpMODULE_DIR)[0];
 $tmpMODULE_UID = explode('.',$tmpMODULE_DIR)[1];
+
+// переопределение путей для обхода мультисайтовости со ссылками 
+$tmpDebrisMODULE_PATH_ABS = explode('/',$tmpMODULE_PATH_ABS);
+$tmpMODULE_PATH = '/'.implode('/',array_slice($tmpDebrisMODULE_PATH_ABS,count($tmpDebrisMODULE_PATH_ABS)-3));
+$tmpMODULE_PATH_ABS = \Bitrix\Main\Application::getDocumentRoot().$tmpMODULE_PATH;
+
 return [
         'PATH_ABS' => $tmpMODULE_PATH_ABS, // абсолютный путь к папке модуля
-        'PATH' => substr($tmpMODULE_PATH_ABS,strlen(\Bitrix\Main\Application::getDocumentRoot())), // путь к папке модуля от корня сайта
+        'PATH' => $tmpMODULE_PATH, // путь к папке модуля от корня сайта
         'DIR' => $tmpMODULE_DIR, // имя папки модуля
         'ID' => $tmpMODULE_DIR, // id модуля
         'SPACE' => $tmpMODULE_SPACE, // пространство модуля (код партнера)
