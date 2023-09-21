42589bb07ab788c8f0671d58025b86dff427a436
Suntechnic <madzhugin@yandex.ru>
2023-09-21 15:41:37 +0300

    Update uninstall.php

--- install/uninstall.php
+++ install/uninstall.php
@@ -1,5 +1,16 @@
 <?
-//\Bitrix\Main\Loader::includeModule('sale');
-//$dctHandlerDirectories = \Bitrix\Sale\PaySystem\Manager::getHandlerDirectories();
-//$targetPath = $dctHandlerDirectories['SYSTEM'].'paysystem';
-//DeleteDirFilesEx($targetPath);
+\Bitrix\Main\Loader::includeModule('sale');
+
+// $dcHandlerDirectories = \Bitrix\Sale\PaySystem\Manager::getHandlerDirectories();
+// $targetPath = $dcHandlerDirectories['SYSTEM'].'paysystemname';
+// DeleteDirFilesEx($targetPath);
+
+// $HandlerDirectories = \Bitrix\Main\Config\Option::get(
+//                 'sale',
+//                 'path2user_ps_files',
+//                 ''
+//             );
+// if ($HandlerDirectories) {
+//     $targetPath = $HandlerDirectories.'paysystemname';
+//     DeleteDirFilesEx($targetPath);
+// }
